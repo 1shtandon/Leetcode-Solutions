88108c707cdace108b0e5d0c56d1880d2b92cb50
1shtandon <202052344@iiitvadodara.ac.in>
2023-02-21 09:47:41 +0530

    New solutions added

---
+++ Array/Single_element_in_a_sorted_array.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int singleNonDuplicate(vector<int> &nums)
+    {
+        int x = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            x ^= nums[i];
+        }
+
+        return x;
+    }
+};
