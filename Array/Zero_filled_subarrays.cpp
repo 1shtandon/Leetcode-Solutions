a2f9714fdea7d248ce444b28f751b647e9de7b49
1shtandon <202052344@iiitvadodara.ac.in>
2023-03-21 10:41:22 +0530

    New solutions added

---
+++ Array/Zero_filled_subarrays.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    long long zeroFilledSubarray(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> zeros(n, 0);
+        if (nums[0] == 0)
+            zeros[0] = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] == 0)
+                zeros[i] = zeros[i - 1] + 1;
+        }
+        long long ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == 0)
+                ans += zeros[i];
+        }
+        return ans;
+    }
+};
