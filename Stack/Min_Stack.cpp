40433c9002856bccbf015c460585909259f590eb
1shtandon <202052344@iiitvadodara.ac.in>
2023-05-13 19:20:11 +0530

    New solutions added

--- Stack/Min_Stack.cpp
+++ Stack/Min_Stack.cpp
@@ -47,3 +47,54 @@ public:
         return st.back().second;
     }
 };
+
+/*
+Approach 2 : Two Stacks
+1. We take two stacks, one for storing the elements and the other for storing the minimum element till that point.
+2. If the element to be pushed is less than or equal to the top element of the minimum stack, then we will push the element in the minimum stack.
+3. On popping, if the element to be popped is equal to the top element of the minimum stack, then we will pop the top element of the minimum stack.
+
+Time Complexity : O(1)
+Space Complexity : O(n)
+
+
+*/
+
+class MinStack
+{
+public:
+    stack<int> st1, st2;
+    MinStack()
+    {
+    }
+
+    void push(int val)
+    {
+        st1.push(val);
+        if (st2.empty() or val <= st2.top())
+        {
+            st2.push(val);
+        }
+    }
+
+    void pop()
+    {
+        if (st1.top() == st2.top())
+        {
+            st2.pop();
+            st1.pop();
+        }
+        else
+            st1.pop();
+    }
+
+    int top()
+    {
+        return st1.top();
+    }
+
+    int getMin()
+    {
+        return st2.top();
+    }
+};
