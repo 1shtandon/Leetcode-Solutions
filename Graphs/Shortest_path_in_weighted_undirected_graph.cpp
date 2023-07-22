e84d2c2a3aacb382e3383a3be04bbf4f353aa156
Vansh <90501023+1shtandon@users.noreply.github.com>
2023-07-22 18:40:21 +0530

    New solutions added

--- Graphs/Shortest_path_in_weighted_undirected_graph.cpp
+++ Graphs/Shortest_path_in_weighted_undirected_graph.cpp
@@ -9,6 +9,7 @@ public:
         for (int i = 0; i < m; i++)
         {
             adj[edges[i][0]].push_back({edges[i][1], edges[i][2]});
+            adj[edges[i][1]].push_back({edges[i][0], edges[i][2]});
         }
 
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
