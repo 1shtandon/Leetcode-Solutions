581c1be431e6eb0f6e0c1bd622984b3cff6a1a94
Vansh <90501023+1shtandon@users.noreply.github.com>
2023-08-16 12:08:56 +0530

    New solutions added

--- Dynamic Programming/Unique_paths.cpp
+++ Dynamic Programming/Unique_paths.cpp
@@ -1,28 +1,35 @@
 class Solution
 {
 public:
+    // int fun(int row , int col , vector<vector<int>> &dp)
+    // {
+    //     if(row == 0 or col == 0)
+    //         return dp[row][col] = 1;
+
+    //     if(dp[row][col] != -1)
+    //         return dp[row][col];
+
+    //     return dp[row][col] = fun(row - 1 , col , dp) + fun(row , col - 1 , dp);
+    // }
+
     int uniquePaths(int m, int n)
     {
-        int dp[m][n];
-        dp[0][0] = 1;
-        // initialization
-        for (int i = 0; i < n; i++)
-        {
-            dp[0][i] = 1;
-        }
-        for (int i = 0; i < m; i++)
-        {
-            dp[i][0] = 1;
-        }
-        // computation
+        // vector<vector<int>> dp(m , vector<int>(n , 0));
+        vector<int> prev(n, 1);
+        vector<int> curr(n, 0);
+
         for (int i = 1; i < m; i++)
         {
-            for (int j = 1; j < n; j++)
+            for (int j = 0; j < n; j++)
             {
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                if (j == 0)
+                    curr[j] = 1;
+                else
+                    curr[j] = prev[j] + curr[j - 1];
             }
+            prev = curr;
         }
-        // ans
-        return dp[m - 1][n - 1];
+
+        return prev[n - 1];
     }
 };
