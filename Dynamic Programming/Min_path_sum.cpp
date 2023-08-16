7a98502ab3981a9789b5417cb1efbc714af30b09
Vansh <90501023+1shtandon@users.noreply.github.com>
2023-08-16 12:56:53 +0530

    New solutions added

--- Dynamic Programming/Min_path_sum.cpp
+++ Dynamic Programming/Min_path_sum.cpp
@@ -1,26 +1,46 @@
 class Solution
 {
 public:
+    // int fun(int row , int col , vector<vector<int>> &grid , vector<vector<int>> &dp)
+    // {
+    //     // base case
+    //     if(row == 0 and col == 0)
+    //         return dp[0][0] = grid[0][0];
+    //     else if(row < 0 or col < 0)
+    //         return INT_MAX;
+
+    //     if(dp[row][col] != -1)
+    //         return dp[row][col];
+
+    //     return dp[row][col] = min(fun(row - 1 , col , grid , dp) , fun(row , col - 1 ,grid , dp)) + grid[row][col];
+    // }
+
     int minPathSum(vector<vector<int>> &grid)
     {
         int rows = grid.size(), cols = grid[0].size();
-        int dp[rows][cols];
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < rows; i++)
-        {
-            dp[i][0] = grid[i][0] + dp[i - 1][0];
-        }
+        // vector<vector<int>> dp(rows , vector<int> (cols , 0));
+        // space optimization
+        vector<int> prev(cols, 0);
+        vector<int> curr(cols, 0);
+
+        prev[0] = grid[0][0];
+
         for (int j = 1; j < cols; j++)
         {
-            dp[0][j] = grid[0][j] + dp[0][j - 1];
+            prev[j] = prev[j - 1] + grid[0][j];
         }
+
         for (int i = 1; i < rows; i++)
         {
+            curr[0] = prev[0] + grid[i][0];
             for (int j = 1; j < cols; j++)
             {
-                dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+                curr[j] = min(prev[j], curr[j - 1]) + grid[i][j];
             }
+
+            prev = curr;
         }
-        return dp[rows - 1][cols - 1];
+
+        return prev[cols - 1];
     }
 };
