9bcc7763c215dc3282a0e2fa0f3d4aff177733c0
Vansh <90501023+1shtandon@users.noreply.github.com>
2023-08-26 14:08:53 +0530

    New solutions added

--- Dynamic Programming/Uncrosses_lines.cpp
+++ Dynamic Programming/Uncrosses_lines.cpp
@@ -1,27 +1,31 @@
-class Solution
-{
+class Solution {
 public:
-    int maxUncrossedLines(vector<int> &nums1, vector<int> &nums2)
-    {
-        int m = nums1.size(), n = nums2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        for (int i = 1; i <= m; i++)
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size() , n = nums2.size();
+        // vector<vector<int>> dp(m + 1 , vector<int> (n + 1 , 0));
+
+        vector<int> prev(n + 1 , 0);
+        vector<int> curr(n + 1 , 0);
+
+        for(int i = 1; i <= m; i++)
         {
-            for (int j = 1; j <= n; j++)
+            for(int j = 1; j <= n; j++)
             {
                 // if elements are equal, then count it
-                if (nums1[i - 1] == nums2[j - 1])
+                if(nums1[i - 1] == nums2[j - 1])
                 {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    curr[j] = 1 + prev[j - 1];
                 }
                 // if not then two conditions, move one step from nums1 or nums2
                 else
                 {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    curr[j] = max(prev[j] , curr[j - 1]);
                 }
             }
+
+            prev = curr;
         }
 
-        return dp[m][n];
+        return prev[n];
     }
 };
