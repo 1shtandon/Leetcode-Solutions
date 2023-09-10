8318a08cf95343e1516b6d38bcb19e15b2217383
Vansh <90501023+1shtandon@users.noreply.github.com>
2023-09-10 17:24:14 +0530

    New solutions added

---
+++ Dynamic Programming/Count All Valid Pickup and Delivery Options.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    // answer is permutation of 2n objects ,and half of those are invalid for each pair
+
+    int countOrders(int n)
+    {
+        long long int mod = 1e9 + 7;
+        long long int ans = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            ans = (ans * (2 * i - 1) * i) % mod;
+        }
+
+        return ans;
+    }
+};
