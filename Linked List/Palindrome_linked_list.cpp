f8aeef05cdffb909fc2ba3f4079ef4e498ab9dd8
Vansh <90501023+1shtandon@users.noreply.github.com>
2023-09-14 14:39:36 +0530

    New solutions added

--- Linked List/Palindrome_linked_list.cpp
+++ Linked List/Palindrome_linked_list.cpp
@@ -30,3 +30,36 @@ public:
         return true;
     }
 };
+
+
+
+class Solution {
+public:
+  bool isPalindrome(ListNode* head) {
+    ListNode *slow = head, *fast = head;
+    ListNode *next, *prev = new ListNode();
+
+    while(fast && fast->next) {
+      // for finding middle
+      slow = slow->next;
+      fast = fast->next->next; 
+
+      // reversing first half
+      next = head->next;
+      head->next = prev;
+      prev = head;
+      head = next;
+    }
+ 
+    // for edge cases
+    if(fast) slow = slow->next;
+    head = prev;
+
+    // comparing first half to second one
+    while(slow) {
+      if(head->val != slow->val) return false;
+      head = head->next, slow = slow->next;
+    }
+    return true;
+  }
+};
